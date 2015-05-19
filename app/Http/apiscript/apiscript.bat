b90dc44d9976638d858008dfb23dd78f2c632373
acjo85 <acjo85@gmail.com>
2015-05-19 15:44:51 +0200

    bat fix

--- app/Http/apiscript/apiscript.bat
+++ app/Http/apiscript/apiscript.bat
@@ -1,2 +1,3 @@
 
 php C:\Users\josephinebr\Desktop\workspace\laravel_event\app\Http\apiscript\apiscript.php
+php C:\Users\Anne-Cathrine\Desktop\Workspace\laravel_event\app\Http\apiscript\apiscript.php
